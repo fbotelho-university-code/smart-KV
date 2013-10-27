99d961904b2c5b35b6d4323ba82d172eb12d109f
Fabio Botelho <balayhashi@gmail.com>
2013-10-27 23:21:17 +0000

    asd

--- Tester/src/datastore/bench/WorkloadPerFlow.java
+++ Tester/src/datastore/bench/WorkloadPerFlow.java
@@ -3,6 +3,13 @@
  */
 package datastore.bench;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+
+import bftsmart.tom.ServiceProxy;
+import bftsmart.tom.TimeoutException;
+import datastore.bench.flowsimulations.FlowSimulation;
+
 /**
  * @author fabiim
  *
@@ -13,7 +20,7 @@ public class WorkloadPerFlow {
 	public final String workload_dsc;
 	public final String name; 
 	public final String datastoreState; 
-	
+	public final byte[][] msgs; 
 	/**
 	 * @param lsw_0_broadcast_ops
 	 * @param lsw_0_brodcast_dsc
@@ -29,6 +36,43 @@ public class WorkloadPerFlow {
 			throw new RuntimeException("Workload : " + name + ":" + workload_dsc  + "Not good");
 		}
 		this.datastoreState = datastoreState; 
+		
+		msgs = new byte[ops.length][];
+		for (int i=0; i < ops.length ; i++){
+			try{
+			ByteArrayOutputStream out = new ByteArrayOutputStream(); 
+			DataOutputStream dos = new DataOutputStream(out);
+			dos.writeInt(ops[i][2]);
+			dos.write(new byte[ops[i][1] -4]);
+			dos.flush(); 
+			dos.close();
+			msgs[i] = out.toByteArray();
+			}catch(Exception e){
+				e.printStackTrace(); 
+				System.exit(-1);
+			}
+			
+		}
 	}
 
+	
+	public void run(ServiceProxy proxy) throws TimeoutException {
+		for (int i=0 ; i < ops.length ; i++){
+			if (isWrite(ops[i])){
+				proxy.invokeOrdered(msgs[i]); 
+			}
+			else{
+				proxy.invokeUnordered(msgs[i]); 
+			}
+		}
+	}
+	
+	private boolean isWrite(int[] is) {
+		return is[0] == FlowSimulation.WRITE_OP; 
+	}
+	
+	public int[][] getFlows() {
+		// TODO Auto-generated method stub
+		return ops;
+	}
 }
